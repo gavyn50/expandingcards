725f0c01c338a2b2f98c43139bc5b43b2e05d7f2
gavyn50 <97207955+gavyn50@users.noreply.github.com>
2022-04-26 08:51:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+const panels = document.querySelectorAll(".panel");
+
+
+const removeActive = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActive();
+    panel.classList.add("active");
+  });
+});
